69a2bb54b2dc278b8bcefdc0b4c7e05c6579e43d
karthik-dasari <karthik.dasari2001@gmail.com>
2023-03-21 10:22:02 +0530

    first commit

--- CitizenSafety/settings.py
+++ CitizenSafety/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
 ]
 
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- CitizenSafety/urls.py
+++ CitizenSafety/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('violation/',include(('violation.urls','violation'),namespace='violation')),
     path('accident/',include(('accident.urls','accident'),namespace='accident')),
     path('issue/',include(('issue.urls','issue'),namespace='issue')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
